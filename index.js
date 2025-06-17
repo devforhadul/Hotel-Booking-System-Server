83b0051740941d0b3c8b3377496e4c77d62c8985
devforhadul <forhadul75@gmail.com>
2025-06-18 00:54:09 +0600

    review backend workd

--- index.js
+++ index.js
@@ -59,11 +59,23 @@ async function run() {
       res.send(result);
     });
 
+    // Get all booked rooms
     app.get('/rooms/booked', async (req, res) => {
       const query = {};
       const result = await hotelBookedDataColl.find(query).toArray();
       res.send(result);
     });
+
+    // get booked room by emamil id
+    app.get('/booked', async (req, res) => {
+      const email = req.query.email;
+      const query = {
+        userEmail: email
+      }
+      const result = await hotelBookedDataColl.find(query).toArray();
+      res.send(result);
+
+    })
 
     // Room sort by top rating
     app.get('/top-rated', async (req, res) => {
@@ -72,10 +84,54 @@ async function run() {
           .sort({ rating: -1 }).limit(6).toArray();
         res.send(result);
       } catch (error) {
-        console.error('❌ Error fetching top-rated rooms:', error);
+        console.error('Error fetching top-rated rooms:', error);
         res.send({ error: 'Failed to fetch top-rated rooms' });
       }
     });
+
+
+    // Delete roommate post
+    app.delete('/booked/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const result = await hotelBookedDataColl.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    //update booked date
+    app.put('/booked/update/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedData = req.body;
+      console.log(updatedData)
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          checkInDate: updatedData.checkInDate,
+          checkOutDate: updatedData.checkOutDate
+        },
+      };
+      const result = await hotelBookedDataColl.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+
+    // add review 
+    app.patch('/rooms/:id/review', async (req, res) => {
+      const id = req.params.id;
+      const reviewData = req.body;
+      const result = await roomsDataColl.updateOne(
+        { _id: new ObjectId(id) },
+        { $push: { reviews: reviewData } }
+      );
+      res.send(result);
+    });
+
+
+
+
+
+
+
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
